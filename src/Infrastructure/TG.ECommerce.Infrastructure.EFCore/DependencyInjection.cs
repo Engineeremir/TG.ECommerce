ad846a661bc593615cb34dfbe09cbf7683badf22
Emirhan <emirhan.kalem320@gmail.com>
2025-09-02 15:26:58 +0300

    ef core layer dependency injection

---
+++ src/Infrastructure/TG.ECommerce.Infrastructure.EFCore/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TG.ECommerce.Domain.AggregateModels.CategoryAggregate;
+using TG.ECommerce.Domain.AggregateModels.ProductAggregate;
+using TG.ECommerce.Infrastructure.EFCore.Contexts;
+using TG.ECommerce.Infrastructure.EFCore.Repositories;
+
+namespace TG.ECommerce.Infrastructure.EFCore;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureEfCore(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<TGECommerceDbContext>(options => options.UseNpgsql("connection string will be provided later"));
+        services.AddRepositories();
+
+        var serviceProvider = services.BuildServiceProvider();
+        var db = serviceProvider.GetRequiredService<TGECommerceDbContext>();
+        db.Database.Migrate();
+
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped(typeof(EfRepository<>));
+
+        return services;
+    }
+}
