efb919392ba2fcdace59f1611df20499f988799e
Emirhan <emirhan.kalem320@gmail.com>
2025-09-02 17:53:19 +0300

    add entity configurations

---
+++ src/Infrastructure/TG.ECommerce.Infrastructure.EFCore/EntityConfigurations/CategoryEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TG.ECommerce.Domain.AggregateModels.CategoryAggregate;
+
+namespace TG.ECommerce.Infrastructure.EFCore.EntityConfigurations
+{
+    public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+            builder.HasKey(t => t.Id);
+            builder.Property(b => b.Id).HasColumnType("UUID").ValueGeneratedNever().IsRequired();
+
+            builder.Property(b => b.Name).HasMaxLength(200);
+            builder.Property(x => x.CreatedOn).IsRequired();
+        }
+    }
+}
---
+++ src/Infrastructure/TG.ECommerce.Infrastructure.EFCore/EntityConfigurations/ProductEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TG.ECommerce.Domain.AggregateModels.ProductAggregate;
+
+namespace TG.ECommerce.Infrastructure.EFCore.EntityConfigurations
+{
+    public class ProductEntityConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+            builder.HasKey(t => t.Id);
+            builder.Property(b => b.Id).HasColumnType("UUID").ValueGeneratedNever().IsRequired();
+
+            builder.Property(b => b.Name).HasMaxLength(200);
+            builder.Property(x => x.CreatedOn).IsRequired();
+        }
+    }
+}
