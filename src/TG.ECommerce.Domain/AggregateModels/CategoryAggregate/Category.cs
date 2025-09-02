563d8c11c7fe23cafb58168a3736ba62b72f3b79
Emirhan <emirhan.kalem320@gmail.com>
2025-09-02 15:14:18 +0300

    create domain entities

---
+++ src/TG.ECommerce.Domain/AggregateModels/CategoryAggregate/Category.cs
@@ -0,0 +1,20 @@
+﻿using TG.ECommerce.Domain.AggregateModels.ProductAggregate;
+using TG.ECommerce.Shared.SeedWork.Entity;
+
+namespace TG.ECommerce.Domain.AggregateModels.CategoryAggregate;
+
+public class Category : Entity
+{
+    public string Name { get; private set; }
+    public ICollection<Product> Products { get; private set; }
+
+    protected Category() {}
+
+    public Category(string name)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+
+        CreatedOn = DateTime.Now;
+    }
+}
---
+++ src/TG.ECommerce.Domain/AggregateModels/ProductAggregate/Product.cs
@@ -0,0 +1,26 @@
+﻿using TG.ECommerce.Domain.AggregateModels.CategoryAggregate;
+using TG.ECommerce.Shared.SeedWork.Entity;
+
+namespace TG.ECommerce.Domain.AggregateModels.ProductAggregate;
+
+public class Product : Entity
+{
+    public string Name { get; private set; }
+    public decimal Price { get; private set; }
+    public int Stock { get; private set; }
+    public Guid CategoryId { get; private set; }
+    public Category Category { get; private set; }
+
+    protected Product() {}
+
+    public Product(string name, decimal price, int stock, Guid categoryId)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Price = price;
+        Stock = stock;
+        CategoryId = categoryId;
+
+        CreatedOn = DateTime.UtcNow;
+    }
+}
