d2720c1e7e2d91372545a7db12453be948972b55
Emirhan <emirhan.kalem320@gmail.com>
2025-09-02 14:56:08 +0300

    base ef core concrete repository with ardalis specification

---
+++ src/Infrastructure/TG.ECommerce.Infrastructure.EFCore/Repositories/EfRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.Specification.EntityFrameworkCore;
+using TG.ECommerce.Infrastructure.EFCore.Contexts;
+using TG.ECommerce.Shared.SeedWork.Repository;
+namespace TG.ECommerce.Infrastructure.EFCore.Repositories;
+
+public class EfRepository<T> : RepositoryBase<T>, IRepository<T> where T : class
+{
+    protected readonly TGECommerceDbContext _context;
+    public IUnitOfWork UnitOfWork => _context;
+
+    public EfRepository(TGECommerceDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
