9194b3ecc54fb097ff7916328bc7e6e8ca8e583b
Emirhan <emirhan.kalem320@gmail.com>
2025-09-02 15:17:55 +0300

    create concrete entity repositories

---
+++ src/Infrastructure/TG.ECommerce.Infrastructure.EFCore/Repositories/CategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using TG.ECommerce.Domain.AggregateModels.CategoryAggregate;
+using TG.ECommerce.Infrastructure.EFCore.Contexts;
+
+namespace TG.ECommerce.Infrastructure.EFCore.Repositories;
+
+public class CategoryRepository : EfRepository<Category>, ICategoryRepository
+{
+    public CategoryRepository(TGECommerceDbContext context) : base(context)
+    {
+    }
+}
---
+++ src/Infrastructure/TG.ECommerce.Infrastructure.EFCore/Repositories/ProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using TG.ECommerce.Domain.AggregateModels.ProductAggregate;
+using TG.ECommerce.Infrastructure.EFCore.Contexts;
+
+namespace TG.ECommerce.Infrastructure.EFCore.Repositories;
+
+public class ProductRepository : EfRepository<Product>, IProductRepository
+{
+    public ProductRepository(TGECommerceDbContext context) : base(context)
+    {
+    }
+}
