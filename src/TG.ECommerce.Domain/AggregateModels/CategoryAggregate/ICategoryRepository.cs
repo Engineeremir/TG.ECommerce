8626c7f6d1e2921cf3cd445801e1c14299b4c7cd
Emirhan <emirhan.kalem320@gmail.com>
2025-09-02 15:16:03 +0300

    create entity repository interfaces

---
+++ src/TG.ECommerce.Domain/AggregateModels/CategoryAggregate/ICategoryRepository.cs
@@ -0,0 +1,7 @@
+﻿using TG.ECommerce.Shared.SeedWork.Repository;
+
+namespace TG.ECommerce.Domain.AggregateModels.CategoryAggregate;
+
+public interface ICategoryRepository : IRepository<Category>
+{
+}
---
+++ src/TG.ECommerce.Domain/AggregateModels/ProductAggregate/IProductRepository.cs
@@ -0,0 +1,7 @@
+﻿using TG.ECommerce.Shared.SeedWork.Repository;
+
+namespace TG.ECommerce.Domain.AggregateModels.ProductAggregate;
+
+public interface IProductRepository : IRepository<Product>
+{
+}
