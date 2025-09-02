f60e400a59f65deed80afa972b775b9bb51db4ce
Emirhan <emirhan.kalem320@gmail.com>
2025-09-02 18:35:19 +0300

    add controllers

---
+++ src/TG.ECommerce.API/Controllers/CategoriesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TG.ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+    }
+}
