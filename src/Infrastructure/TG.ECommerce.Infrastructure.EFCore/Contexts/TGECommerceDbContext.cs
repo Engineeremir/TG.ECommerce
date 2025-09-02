3397a508eaac5a43885188cf12b502dd5b4c20bf
Emirhan <emirhan.kalem320@gmail.com>
2025-09-02 14:51:00 +0300

    design ef core db context with postgre

---
+++ src/Infrastructure/TG.ECommerce.Infrastructure.EFCore/Contexts/TGECommerceDbContext.cs
@@ -0,0 +1,53 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TG.ECommerce.Shared.SeedWork.Repository;
+
+namespace TG.ECommerce.Infrastructure.EFCore.Contexts;
+
+public class TGECommerceDbContext : DbContext, IUnitOfWork
+{
+    public const string DefaultSchema = "public";
+    private readonly IMediator _mediator = null!;
+
+    public TGECommerceDbContext()
+    {
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimeStampBehaviour", true);
+        AppContext.SetSwitch("Npgsql.EnableDiscardEvents", false);
+    }
+
+    public TGECommerceDbContext(DbContextOptions<TGECommerceDbContext> options) : base(options)
+    {
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimeStampBehaviour", true);
+        AppContext.SetSwitch("Npgsql.EnableDiscardEvents", false);
+    }
+
+    public TGECommerceDbContext(DbContextOptions<TGECommerceDbContext> options, IMediator mediator) : base(options)
+    {
+        _mediator = mediator;
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimeStampBehaviour", true);
+        AppContext.SetSwitch("Npgsql.EnableDiscardEvents", false);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.EnableSensitiveDataLogging();
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TGECommerceDbContext).Assembly);
+    }
+
+    public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+    {
+        int result = await base.SaveChangesAsync(cancellationToken);
+
+        if (result > 0)
+        {
+            //dispatch domain events
+        }
+
+        return true;
+    }
+}
